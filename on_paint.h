28173fbff51dc39f8ad7ca7577c184d69c77eb3d
Dejan Budimir <dejbug@users.noreply.github.com>
2016-07-08 16:03:32 +0200

    ok

--- on_paint.h
+++ on_paint.h
@@ -13,35 +13,25 @@ extern context_t defcon;;
 void on_paint(HWND h, UINT m, WPARAM w, LPARAM l)
 {
 	PAINTSTRUCT ps;
-	HDC const& hdc = ps.hdc;
 	
 	BeginPaint(h, &ps);
-	
-	mdc2_t mdc(hdc);
-	
+	mdc2_t mdc(ps.hdc);
 	mdc.clear(COLOR_BTNFACE);
-	//~ mdc.clear(color_t(70, true, true));
-	
-	//~ temp_object_t brush(CreateSolidBrush(RGB(200,100,100)));
-	//~ FillRect(mdc.handle, &r.rect, (HBRUSH)brush.handle);
 	
 	defcon.canvas.draw_area(mdc.handle);
 	
 	temp_color_setter_t cs(mdc.handle);
 	
-	cs.fg(255,200,200);
+	//~ cs.fg(255,200,200);
+	cs.fg(color_t(30*11,.20f,.9f).c);
 	cs.bg(false);
 	
 	defcon.tracker.grid.draw(mdc.handle);
 	
 	cs.reset();
 	
-	line_t line(100,100,600,100);
-	line.draw(mdc.handle);
-	
 	defcon.canvas.draw_border(mdc.handle);
 	
 	mdc.flip();
-	
 	EndPaint(h, &ps);
 }
